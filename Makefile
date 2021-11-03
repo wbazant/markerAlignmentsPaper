9d3d36ad95136009690a11ba830fdc47b94bcc0c
Wojtek Bazant <wbazant@vet.upenn.edu>
2021-11-03 07:40:05 -0400

    Somewhat working implementation of misassignment classifier

--- Makefile
+++ Makefile
@@ -16,14 +16,14 @@ tmp:
 	mkdir -pv tmp
 
 tmp/wgsimMutationRate.json: tmp refdb/ncbi_eukprot_met_arch_markers.fna
-	python3 scripts/simulate_and_align.py --refdb-marker-to-taxon-path refdb/busco_taxid_link.txt --reference refdb/ncbi_eukprot_met_arch_markers.fna --sim-source refdb/ncbi_eukprot_met_arch_markers.fna --dir tmp --verbose --read-lengths 100 --base-error-rates 0.0 --mutation-rates 0.0 0.001 0.01 0.05 0.075 0.1 0.125 0.15 0.2 > tmp/wgsimMutationRate.json
+	python3 scripts/simulate_and_align.py --refdb-ncbi refdb/taxa.sqlite --refdb-marker-to-taxon-path refdb/busco_taxid_link.txt --reference refdb/ncbi_eukprot_met_arch_markers.fna --sim-source refdb/ncbi_eukprot_met_arch_markers.fna --dir tmp --verbose --read-lengths 100 --base-error-rates 0.0 --mutation-rates 0.0 0.001 0.01 0.05 0.075 0.1 0.125 0.15 0.2 > tmp/wgsimMutationRate.json
 
 
 tmpLeaveOneOut:
 	mkdir -pv tmpLeaveOneOut
 
 tmpLeaveOneOut/wgsimMutationRateLeaveOneOut.json: tmpLeaveOneOut refdbCrossValidation/nineTenth.fna.1.bt2
-	python3 scripts/simulate_and_align.py --refdb-marker-to-taxon-path refdb/busco_taxid_link.txt --reference refdbCrossValidation/nineTenth.fna --sim-source refdbCrossValidation/oneTenth.fna --dir tmpLeaveOneOut --verbose --read-lengths 100 --base-error-rates 0.0 --mutation-rates 0.0 0.01 0.05 > tmpLeaveOneOut/wgsimMutationRateLeaveOneOut.json
+	python3 scripts/simulate_and_align.py --refdb-ncbi refdb/taxa.sqlite --refdb-marker-to-taxon-path refdb/busco_taxid_link.txt --reference refdbCrossValidation/nineTenth.fna --sim-source refdbCrossValidation/oneTenth.fna --dir tmpLeaveOneOut --verbose --read-lengths 100 --base-error-rates 0.0 --mutation-rates 0.0 0.01 0.05 > tmpLeaveOneOut/wgsimMutationRateLeaveOneOut.json
 
 figures:
 	mkdir -pv figures
--- scripts/simulate_and_align.py
+++ scripts/simulate_and_align.py
@@ -8,6 +8,8 @@ import json
 import statistics
 import logging
 import subprocess
+from ete3 import NCBITaxa
+
 from pathlib import Path
 
 
@@ -26,17 +28,18 @@ def main_loop(**kwargs):
    
     marker_to_taxon = read_marker_to_taxon(kwargs['refdb_marker_to_taxon_path'])
 
+    ncbi = NCBITaxa(kwargs['refdb_ncbi'])
     for mutation_rate in kwargs['mutation_rates']:
         for base_error_rate in kwargs['base_error_rates']:
             for read_length in kwargs['read_lengths']:
-                d = do_one(marker_to_taxon, read_length, base_error_rate, mutation_rate, **kwargs)
+                d = do_one(ncbi, marker_to_taxon, read_length, base_error_rate, mutation_rate, **kwargs)
                 d['mutation_rate'] = mutation_rate
                 d['base_error_rate'] = base_error_rate
                 d['read_length'] = read_length
                 result.append(d)
     print(json.dumps(result, indent = 4))
 
-def do_one(marker_to_taxon, read_length, base_error_rate, mutation_rate, wd, logger, ref_db, sim_source, seed, **kwargs):
+def do_one(ncbi, marker_to_taxon, read_length, base_error_rate, mutation_rate, wd, logger, ref_db, sim_source, seed, **kwargs):
     Path(wd).mkdir(parents=True, exist_ok=True)
 
     prefix = f"{read_length}.{base_error_rate}.{mutation_rate}"
@@ -75,7 +78,7 @@ def do_one(marker_to_taxon, read_length, base_error_rate, mutation_rate, wd, log
 
     if not os.path.isfile(summary_path):
         logger.info("Summarizing " + " ".join([sam_path, sim_path_1]))
-        summary = summarize_sim_alignment(marker_to_taxon, sam_path)
+        summary = summarize_sim_alignment(ncbi, marker_to_taxon, sam_path)
         grep_cmd = ['grep', '-c', '^@', sim_path_1]
         logger.info("Running: " + " ".join(grep_cmd))
         summary["numQueriesTotal"] = int(subprocess.run(grep_cmd, stdout = subprocess.PIPE).stdout.decode(encoding="utf-8").replace("\n", ""))
@@ -109,7 +112,32 @@ pattern_busco = re.compile(eukprot_refdb_regex_busco)
 def next_g(search):
     return next(g for g in search.groups() if g is not None)
 
-def match(marker_to_taxon, query_name, reference_name):
+trad_ranks = {"superkingdom", "kingdom", "phylum", "class", "order", "family", "genus", "species"}
+
+match_type_cache = {}
+def get_match_type(ncbi, source_taxid, matched_taxid):
+    k = (source_taxid, matched_taxid)
+    if k in match_type_cache:
+        return match_type_cache[k]
+    tree = ncbi.get_topology([source_taxid, matched_taxid])
+
+    r = tree.get_tree_root()
+    if hasattr(r, 'rank') and r.rank in trad_ranks:
+        result = r.rank
+    else:
+        lineage = ncbi.get_lineage(r.name)
+        ranks = ncbi.get_rank(lineage)
+        ranks_increasing = [ranks[x] for x in reversed(lineage) if ranks[x] in trad_ranks]
+
+        if not ranks_increasing:
+            raise ValueError(lineage, ranks)
+        result = ranks_increasing[0]
+
+
+    match_type_cache[k] = result
+    return result
+
+def match(ncbi, marker_to_taxon, query_name, reference_name):
     # not a cross-match if the read aligns to the other copy of the bu(m)sco
 
     source_taxon_search = pattern_taxon.search(query_name) 
@@ -144,16 +172,31 @@ def match(marker_to_taxon, query_name, reference_name):
 
     source_taxid = marker_to_taxon[source_busco]
     matched_taxid = marker_to_taxon[matched_busco]
-    
-    return source_taxid, source_busco, matched_taxid, matched_busco, is_match
 
-def summarize_sim_alignment(marker_to_taxon, input_alignment_file):
+    if is_match:
+        match_type = 'true_match'
+    else:
+        match_type = get_match_type(ncbi, source_taxid, matched_taxid)
+
+    is_corresponding_markers = source_marker == matched_marker
+
+    if match_type == 'species' and is_corresponding_markers:
+        raise ValueError(source_busco, matched_busco, source_marker)
+
+
+    return source_taxid, source_busco, matched_taxid, matched_busco, match_type
+
+def is_one_element_dict(d, required_element):
+    return len(d) == 1 and required_element in d
+
+def summarize_sim_alignment(ncbi, marker_to_taxon, input_alignment_file):
     alignment_file = pysam.AlignmentFile(input_alignment_file, check_sq=False)
 
     queries = {}
     matched_buscos = {}
     mapqs = {}
     query_lengths = {}
+    match_types = {}
 
     identities_match_true = []
     identities_match_false = []
@@ -164,15 +207,21 @@ def summarize_sim_alignment(marker_to_taxon, input_alignment_file):
         mapq = read.mapq
         query_length = read.infer_query_length()
 
-        source_taxon, source_busco, matched_taxon, matched_busco, is_match = match(marker_to_taxon, query, read.reference_name)
+        source_taxon, source_busco, matched_taxon, matched_busco, match_type = match(ncbi, marker_to_taxon, query, read.reference_name)
 
         if matched_busco not in matched_buscos:
-            matched_buscos[matched_busco] = {True: 0, False: 0}
-        matched_buscos[matched_busco][is_match] += 1
+            matched_buscos[matched_busco] = {}
+
+        if match_type not in matched_buscos[matched_busco]:
+            matched_buscos[matched_busco][match_type] = 0
+
+        matched_buscos[matched_busco][match_type] += 1
 
         if query not in queries:
-            queries[query] = {True: 0, False: 0}
-        queries[query][is_match] += 1
+            queries[query] = {}
+        if match_type not in queries[query]:
+            queries[query][match_type] = 0
+        queries[query][match_type] += 1
 
         if mapq not in mapqs:
             mapqs[mapq] = 0
@@ -182,7 +231,11 @@ def summarize_sim_alignment(marker_to_taxon, input_alignment_file):
             query_lengths[query_length] = 0
         query_lengths[query_length] += 1
 
-        if is_match:
+        if match_type not in match_types:
+            match_types[match_type] = 0
+        match_types[match_type] += 1
+
+        if match_type == 'true_match':
             identities_match_true.append(identity)
         else:
             identities_match_false.append(identity)
@@ -191,7 +244,6 @@ def summarize_sim_alignment(marker_to_taxon, input_alignment_file):
     mapqs_n = 0.0
     mapqs_n_at_least_30 = 0.0
     mapqs_sum = 0.0
-
     if 42 not in mapqs:
         mapqs[42] = 0
 
@@ -216,9 +268,15 @@ def summarize_sim_alignment(marker_to_taxon, input_alignment_file):
     query_lengths_avg = query_lengths_sum / query_lengths_n if query_lengths_n else 0
     return {
         "numQueriesMapped": len (queries.keys()),
-        "numQueriesMappedOnlyAsMatch": len ([k for k in queries if queries[k][True] > 0 and queries[k][False] == 0 ]),
-        "numBuscosMapped": len (matched_buscos.keys()),
-        "numBuscosMappedOnlyAsMatch": len ([k for k in matched_buscos if matched_buscos[k][True] > 0 and matched_buscos[k][False] == 0 ]),
+        "numQueriesMappedOnlyAsMatch": len ([k for k in queries if is_one_element_dict(queries[k], 'true_match')]),
+        "numQueriesMappedOnlyAsSameSpecies": len ([k for k in queries if is_one_element_dict(queries[k], 'species')]),
+        "numQueriesMappedOnlyAsSameGenus": len ([k for k in queries if is_one_element_dict(queries[k], 'genus')]),
+        "numQueriesMappedOnlyAsSameFamily": len ([k for k in queries if is_one_element_dict(queries[k], 'family')]),
+        "numBuscosMapped": len (matched_buscos),
+        "numBuscosMappedOnlyAsMatch": len ([k for k in matched_buscos if is_one_element_dict(matched_buscos[k], 'true_match')]),
+        "numBuscosMappedOnlyAsSameSpecies": len ([k for k in matched_buscos if is_one_element_dict(matched_buscos[k], 'species')]),
+        "numBuscosMappedOnlyAsSameGenus": len ([k for k in matched_buscos if is_one_element_dict(matched_buscos[k], 'genus')]),
+        "numBuscosMappedOnlyAsSameFamily": len ([k for k in matched_buscos if is_one_element_dict(matched_buscos[k], 'family')]),
         "meanIdentitiesMatchTrue": statistics.mean(identities_match_true) if identities_match_true else 0.0,
         "meanIdentitiesMatchFalse": statistics.mean(identities_match_false) if identities_match_false else 0.0,
         "mapqsAvg": mapqs_avg,
@@ -246,6 +304,7 @@ def main(argv=sys.argv[1:]):
     parser.add_argument("--mutation-rates", nargs='+', type=float,action = "store",  dest = "mutation_rates", required = True)
     parser.add_argument("--seed", type=int, default = 1337)
     parser.add_argument("--refdb-marker-to-taxon-path", type=str, action="store", dest="refdb_marker_to_taxon_path", help = "Lookup file, two columns - marker name, taxon name")
+    parser.add_argument("--refdb-ncbi", type=str, action="store", dest="refdb_ncbi", help = "argument for ete.NCBITaxa")
 
     options=parser.parse_args(argv)
 

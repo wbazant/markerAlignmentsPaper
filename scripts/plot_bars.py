a09b0833c7318d2b0d658f1760e35f9c25a3957e
Wojtek Bazant <wbazant@vet.upenn.edu>
2021-11-08 08:50:32 -0500

    One big figure with bars

--- scripts/plot_bars.py
+++ scripts/plot_bars.py
@@ -18,24 +18,22 @@ def main(argv=sys.argv[1:]):
     df['mutation_rate'] = ["{:.3f}".format(x) for x in df['mutation_rate']]
     
 
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    fig.tight_layout()
-
-    df1 = pandas.DataFrame({'numQueriesTotal': df['numQueriesTotal'], 'numQueriesMapped': df['numQueriesMapped'], 'numQueriesMappedOnlyAsSameFamily': df['numQueriesMappedOnlyAsSameFamily'], 'numQueriesMappedOnlyAsSameGenus': df['numQueriesMappedOnlyAsSameGenus'], 'numQueriesMappedOnlyAsSameSpecies': df['numQueriesMappedOnlyAsSameSpecies'], 'mutation_rate' : df['mutation_rate'] })
-
-    # skip two points so the distances are even in mutation rate
-    #df = df.drop(axis="index", labels=[1,2])
-    
-    df1.plot.bar(x='mutation_rate', ax=ax1)
-
-    df2 = pandas.DataFrame({'numQueriesTotal': df['numQueriesTotal'],
-        'numQueriesMapped': df['numQueriesMapped'],
+    df = pandas.DataFrame({'numQueriesTotal': df['numQueriesTotal'], 'numQueriesMapped': df['numQueriesMapped'], 'numQueriesMappedOnlyAsSameFamily': df['numQueriesMappedOnlyAsSameFamily'], 'numQueriesMappedOnlyAsSameGenus': df['numQueriesMappedOnlyAsSameGenus'], 'numQueriesMappedOnlyAsSameSpecies': df['numQueriesMappedOnlyAsSameSpecies'],
         'numQueriesMappedWhenMapqAtLeast30': df['numQueriesMappedWhenMapqAtLeast30'],
         'numQueriesMappedOnlyAsSameFamilyWhenMapqAtLeast30': df['numQueriesMappedOnlyAsSameFamilyWhenMapqAtLeast30'], 'numQueriesMappedOnlyAsSameGenusWhenMapqAtLeast30': df['numQueriesMappedOnlyAsSameGenusWhenMapqAtLeast30'], 'numQueriesMappedOnlyAsSameSpeciesWhenMapqAtLeast30': df['numQueriesMappedOnlyAsSameSpeciesWhenMapqAtLeast30'],
         'mutation_rate' : df['mutation_rate'] })
-    df2.plot.bar(x='mutation_rate', ax=ax2)
 
-    fig.savefig(options.output_path, bbox_inches='tight')
+    # skip two points so the distances are even in mutation rate
+    #df = df.drop(axis="index", labels=[1,2])
+
+    ax = df.plot.bar(x='mutation_rate')
+
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.3),
+          ncol=2, fancybox=True, shadow=True)
+
+    fig = ax.get_figure()
+
+    fig.savefig(options.output_path, bbox_inches='tight', dpi=199)
 
 if __name__ == '__main__':
     main()

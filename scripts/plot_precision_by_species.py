1e03a169a9f011e3e58d865d94f926bc10134244
Wojtek Bazant <wbazant@vet.upenn.edu>
2021-11-25 06:30:38 -0500

    Put the annoyingly overplotted plot in for now

--- scripts/plot_precision_by_species.py
+++ scripts/plot_precision_by_species.py
@@ -32,15 +32,37 @@ def do(input_db, output_png, refdb_ncbi):
 
     ncbi = NCBITaxa(refdb_ncbi)
     df = pandas.read_sql_query(sql, sqlite3.connect(input_db))
-    ax = df.plot.scatter(x='fraction_mapq_at_least_30', y='precision', alpha=0.3)
+    df['kingdom'] = [get_kingdom(ncbi, x) for x in df['source_taxon']]
+
+
+    fig, ax = plt.subplots()
+# no legend!
+#    colors = { "Metazoa": "yellow", "Viridiplantae": "blue", "Fungi": "green", "Other": "grey"}
+#    df['kingdomColors'] = [colors[get_kingdom(ncbi, x)] for x in df['source_taxon']]
+#    df.plot.scatter(x='fraction_mapq_at_least_30', y='precision', alpha=0.3,c='kingdomColors', ax=ax)
+
+# annoyingly overplotted
+    for name, group in [t for t in df.groupby("kingdom")]:
+      plt.plot(group['fraction_mapq_at_least_30'], group['precision'], marker="x", linestyle="", label=name, alpha=0.2)
+
+    ax.legend()
     ax.set_xlabel("Fraction reads with MAPQ >= 30")
     ax.set_ylabel("Precision")
-    fig = ax.get_figure()
     fig.savefig(output_png, bbox_inches='tight', dpi=199)
     
+def get_kingdom(ncbi, taxid):
+    lineage = ncbi.get_lineage(taxid)
+    names = ncbi.get_taxid_translator(lineage)
+    d = ncbi.get_rank(lineage)
+    ks = [k for k in d if d[k] == 'kingdom']
+    if len(ks) == 1:
+      return names[ks[0]]
+    if 2759 in d:
+        return "Other"
+    raise ValueError(d)
 
 
-def main(argv=sys.argv[1:]):
+def opts(argv):
     parser = argparse.ArgumentParser(
       description="plot precision by species",
       formatter_class = argparse.RawDescriptionHelpFormatter,
@@ -49,9 +71,12 @@ def main(argv=sys.argv[1:]):
     parser.add_argument("--output-png", type=str, action="store", dest="output_png", required=True)
     parser.add_argument("--refdb-ncbi", type=str, action="store", dest="refdb_ncbi", help = "argument for ete.NCBITaxa")
 
-    options=parser.parse_args(argv)
+    return parser.parse_args(argv)
+
+def main(argv=sys.argv[1:]):
 
 
+    options = opts(argv)
     do(**vars(options))
     
 
